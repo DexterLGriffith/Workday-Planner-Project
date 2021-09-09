15a21335c569da1da8ddd4c28430cabf5bad12be
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-09-08 22:44:52 -0500

    added date stamp, still working on colors and save feature

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+var date = new Date();
+document.getElementById("currentDay").innerHTML = date.toLocaleDateString('en-US', {
+    weekday: "long",
+    day: "numeric",
+    year: "numeric",
+    month: "long",
+});
+
+
+function placeholder() {
+    var savedAppointment = document.getElementById()
+}
