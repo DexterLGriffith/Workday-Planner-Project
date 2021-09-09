cf69ef778d33e85b0cecd3dd835cf869e90a3a8f
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-09-09 01:32:32 -0500

    changed up the columsn to add bootstrap columns... got the hour CSS to funciton correctly. focus on color change and save feature next.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,10 @@
+// date and time
 var date = new Date();
 document.getElementById("currentDay").innerHTML = date.toLocaleDateString('en-US', {
     weekday: "long",
     day: "numeric",
     year: "numeric",
     month: "long",
+    hour: "numeric",
+    minute: "numeric",
 });
-
-
-function placeholder() {
-    var savedAppointment = document.getElementById()
-}
