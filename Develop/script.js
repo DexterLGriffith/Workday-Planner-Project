29ef35e63ba0bc28831519f44c13c7450b560a42
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-09-10 00:06:38 -0500

    attempt at background color change and save feature.

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,31 @@
 // date and time
 var date = moment();
 $("#currentDay").text(date.format("MMMM Do, YYYY, HH:MM"));
+var timeblock1 = document.getElementById("#timeblock1");
 
-
-
-var timeDay = $(".hour");
-var now = parseInt(moment().format());
-
-$.each(timeDay, function (i, hour) {
-    var timeD = parseInt($(this).attr("id"));
-    if (timeD === now) {
-        $(this).next().addClass("present");
-
+// ---------past,present,future background color change attempt ------//
+var timeDay = date.getHours;
+    if (timeblock1) {
+        if (timeDay === moment) {
+            next().addClass("present");
+        }
     }
-    else if (timeD < now) {
-        $(this).next().addClass("past");
+    else if (timeblock1) { 
+        if (timeDay > moment) {
+            next().addClass("future");
+        }
     }
-    else if (timeD > now) {
-        $(this).next().addClass("future");
+    else if (timeblock1) {
+        if (timeDay < moment) {
+            next().addClass("past");
+        }
     }
-});
+// --------save button to local storage-----//
+var SaveBtn = document.getElementById("saveBtn");
+SaveBtn.onclick = saveData();
+
+function saveData() {
+    var input = document.getElementById("text");
+    localStorage.setItem("text", input.placeholder);
+   
+}
